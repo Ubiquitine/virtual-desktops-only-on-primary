73644e92831d0554bcc38f187130c68e90d737c5
Francois @fanf42 Armand <francois@fanf.eu>
2024-08-22 14:42:04 +0200

    Fixes #4: add a missing closing brace in list-screens.js

--- contents/code/list-screens.js
+++ contents/code/list-screens.js
@@ -1,3 +1,4 @@
 for (var i = 0; i < workspace.screens.length; i++) {
     var screen = workspace.screens[i];
     print("Screen #" + i + ": Name: " + screen.name + ", Brand: " + screen.manufacturer + ", Model: " + screen.model);
+}
